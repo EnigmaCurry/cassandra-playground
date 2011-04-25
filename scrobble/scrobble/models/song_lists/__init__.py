2029e78688623de5b447756848bc2687b610e587
Ryan McGuire <ryan@enigmacurry.com>
2011-04-25 00:50:06 -0400

    delted a few files

--- scrobble/scrobble/models/song_lists/__init__.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-def load_song_list(path):
-    with open(os.path.join("song_lists","helios.txt")) as f:
-        for line in f:
-            parts = line.strip().split("|")
-            d = dict(artist=parts[0],album=parts[1],title=parts[2])
-            yield d
