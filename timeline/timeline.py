e36c24e1a0227c73468dc25842aa88d707842a47
Ryan McGuire <ryan@enigmacurry.com>
2011-05-11 23:41:55 -0400

    Add more test cases.

--- timeline/timeline.py
+++ timeline/timeline.py
@@ -33,6 +33,7 @@ import pycassa
 from time import mktime
 import random
 from datetime import datetime, timedelta
+import unittest
 
 class Timeline(object):
     def __init__(self, column_family):
@@ -50,6 +51,8 @@ class Timeline(object):
     def get_events_between(self, start, finish):
         "Return all the events in the given date range"
         events = []
+        #Range should be non-inclusive of finish, so subtract one millisecond:
+        finish = finish - timedelta(milliseconds=1)
         #Iterate over each day from start to finish:
         #Start with start stripped of it's hours, minutes, seconds:
         date = datetime(*start.timetuple()[:3])
@@ -76,47 +79,90 @@ class Timeline(object):
     def __datetime_to_cf_key(self, date):
         "Create a column family key from a datetime"
         return date.strftime("%Y-%m-%d")
-    
-def generate_events(n=100, start=None, finish=None):
-    "Generate n events on random dates between start and finish."
-    if start is None:
-        start = datetime(2011,1,1)
-    if finish is None:
-        finish = datetime(2012,1,1)
-    start = mktime(start.timetuple())
-    finish = mktime(finish.timetuple())
-    for i in xrange(n):
-        d = datetime.fromtimestamp(random.randint(start,finish))
-        yield (d,"Event {0}".format(i+1))
 
-def test():
-    pool = pycassa.connect("Timeline")
-    cf = pycassa.ColumnFamily(pool, "timeline")
-    cf.truncate()
-    
-    timeline = Timeline(cf)
+class TimelineTest(unittest.TestCase):
+    def setUp(self):
+        self.pool = pycassa.connect("Timeline")
+        self.cf = pycassa.ColumnFamily(self.pool, "timeline")
+        self.cf.truncate()
 
-    #Generate 100 random events for May 2011.
-    #Keep the event list around so we can verify them later:
-    events = list(generate_events(100,start=datetime(2011,5,1),
-                                  finish=datetime(2011,6,1)))
-    #Add these events to the timeline:
-    for date, msg in events:
-        timeline.append(msg, time=date)
-    #Also add some events for the rest of the year to add noise:
-    for date, msg in generate_events(400, start=datetime(2011,1,1),
-                             finish=datetime(2011,5,1)):
-        timeline.append(msg, time=date)
-    for date, msg in generate_events(400, start=datetime(2011,6,1),
-                             finish=datetime(2012,1,1)):
-        timeline.append(msg, time=date)
+    def generate_events(self, n=100, start=None, finish=None):
+        "Generate n events on random dates between start and finish."
+        if start is None:
+            start = datetime(2011,1,1)
+        if finish is None:
+            finish = datetime(2012,1,1)
+        start = mktime(start.timetuple())
+        finish = mktime(finish.timetuple())
+        for i in xrange(n):
+            d = datetime.fromtimestamp(random.randint(start,finish))
+            yield (d,"Event {0}".format(i+1))
 
-    #Now get all the events for May back in order:
-    may_events = timeline.get_events_between(datetime(2011,5,1),
-                                             datetime(2011,6,1))
-    #Ensure that the events are the same, and that they are in order:
-    assert may_events == sorted(events)
-    print may_events
+    def writeRandomEvents(self, timeline, preseed_events=None):
+        #Generate 100 random events for May 2011.
+        #Keep the event list around so we can verify them later:
+        events = list(self.generate_events(100,start=datetime(2011,5,1),
+                                           finish=datetime(2011,6,1)))
+        if preseed_events:
+            events.extend(preseed_events)
+        #Add these events to the timeline:
+        for date, msg in events:
+            timeline.append(msg, time=date)
+        #Also add some events for the rest of the year to add noise:
+        for date, msg in self.generate_events(400, start=datetime(2011,1,1),
+                                         finish=datetime(2011,5,1)):
+            timeline.append(msg, time=date)
+        for date, msg in self.generate_events(400, start=datetime(2011,6,1),
+                                         finish=datetime(2012,1,1)):
+            timeline.append(msg, time=date)
+        return events
     
-if __name__ == "__main__":
-    test()
+    def testEventOrder(self):
+        "Test writing and retrieving of events in chronological order"
+        timeline = Timeline(self.cf)
+        events = self.writeRandomEvents(timeline)
+        #Now get all the events for May back in order:
+        may_events = timeline.get_events_between(datetime(2011,5,1),
+                                                 datetime(2011,6,1))
+        #Ensure that the events are the same, and that they are in chron order:
+        self.assertEqual(may_events,sorted(events))
+
+    def testDateExtremeties(self):
+        """Make sure events just inside/outside the date range are
+        (or are not) included."""
+        timeline = Timeline(self.cf)
+        #Add a few hand crafted events for date extremities:
+        first_event = (datetime(2011,5,1), "Very First Event of May")
+        last_event = (datetime(2011,6,1)-timedelta(seconds=1),
+                       "Very Last Event of May")
+        march_event = (datetime(2011,5,1)-timedelta(seconds=1),
+                       "Very Last Event of March")
+        june_event = (datetime(2011,6,1), "Very First Event of June")
+        events = [first_event,last_event]
+        events = self.writeRandomEvents(timeline, events)
+        #Now get all the events for May back in order:
+        may_events = timeline.get_events_between(datetime(2011,5,1),
+                                                 datetime(2011,6,1))
+        self.assertEqual(may_events[0], first_event)
+        self.assertEqual(may_events[-1], last_event)
+        self.assertTrue(march_event not in may_events)
+        self.assertTrue(june_event not in may_events)
+        
+    def testMultiClients(self):
+        "Simulate multiple clients by writing the same events twice"
+        timeline = Timeline(self.cf)        
+        events = list(self.generate_events(100,start=datetime(2011,5,1),
+                                           finish=datetime(2011,6,1)))
+        #Write the event once:
+        for date, event in events:
+            timeline.append(event, time=date)
+        #Write the event twice:
+        for date, event in events:
+            timeline.append(event, time=date)
+        #Now each event should be in the db twice, simulating two
+        #clients writing the same events at the same time:
+        may_events = timeline.get_events_between(datetime(2011,5,1),
+                                                 datetime(2011,6,1))
+        self.assertEqual(len(may_events), 200)
+        
+        
